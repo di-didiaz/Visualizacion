6dc3427e144c3f91c0cad5cbe2afb0421635862d
DiDi <dcdiazgo@uoc.edu>
2025-12-11 16:36:44 +0000

    63

--- aplicacion.py
+++ aplicacion.py
@@ -55,7 +55,6 @@ if selected == "Resultados de la encuesta":
     with tab1:
 
         st.header("Composición fisica por comunidad autonoma")
-        st.write("Haz click para ver detalles por comunidad")
         st.caption(" El IMC es la medida de XYZ importante porque XYZ") 
 
         df_mapa= df.groupby("Comunidad Autonoma").agg(IMC_prom=('IMC','mean'),peso_prom=('Peso','mean'), edad_prom= ('Edad','mean'), n=('IMC','count')).reset_index()
@@ -69,31 +68,96 @@ if selected == "Resultados de la encuesta":
 
         mapaccaa= px.choropleth(df_mapa, geojson= geoson_mapa, locations="Comunidad Autonoma",
                                 featureidkey="properties.name", color="IMC_prom",hover_name="Comunidad Autonoma", hover_data={'IMC_prom', 'peso_prom', 'edad_prom'}, labels={'IMC_prom': 'Promedio IMC'},
-                                title="IMC medio por comunidad autonoma", color_continuous_scale="Viridis")
+                                title="IMC medio por comunidad autonoma. Haz click y zoom para ver detalles por comunidad", color_continuous_scale="Viridis")
         mapaccaa.update_geos(fitbounds="locations", visible=False)
         mapaccaa.update_layout(margin={'r':0,'l':0, 'b':0,'t':50})
-        select=st.plotly_chart(mapaccaa,use_container_width=True)
+        select=st.plotly_chart(mapaccaa,width='content')
 
+        st.markdown("----------------")
+
+        st.subheader("Mas detalles:") 
         com_selec=st.selectbox("Selecciona o busca una comunidad de tu interes:", options=[""]+ df['Comunidad Autonoma'].dropna().unique().tolist())
         if com_selec:
             ca_elegida= df[df["Comunidad Autonoma"]==com_selec]
             tabla_ca=ca_elegida.groupby('Sexo').agg(sexo=('Sexo','count'), peso_ca= ('Peso', 'mean'), imc_ca= ('IMC','mean')).reset_index()
             st.subheader(f"Resultados de {com_selec}")
-            st.dataframe(tabla_ca)
+            st.dataframe(tabla_ca) #https://www.youtube.com/watch?v=7E3yxq-P-a8
 
         
  ###### Tab 2 #######       
     with tab2:
 
 
-        st.subheader("Horas del dia sentado en los encuestados")
+        st.subheader("Salud percibida y cronicidad por sexo")
+        st.caption("Los encuestados podian reportar el sexo con el que se identificaban y el estado de salud como 'Muy bueno', 'Bueno','Regular','Malo'y 'Muy malo'")
+        salud_per=df[df['Salud_Percibida'].notna()]
+        
+        mb_bueno= salud_per['Salud_Percibida'].str.contains('Bueno', case=False, na=False)
+        buenos= salud_per.groupby('Sexo').apply(lambda x:mb_bueno.loc[x.index].mean()*100)
+       
+        cronicidad=df[df['Cronicidad_bin'].notna()]
+        cont_cronicidad= cronicidad.groupby('Sexo')['Cronicidad_bin'].mean()*100
+
+        sexos= list(buenos.index)
+        st.write("Buena salud percibida por sexos reportados")
+        col21, col22= st.columns(2)
+        
+        col21.metric(label=f"{sexos[0]}", value= f"{buenos.loc[sexos[0]]:.2f}%" )
+        col22.metric(f"{sexos[1]}",f"{buenos.loc[sexos[1]]:.2f}%" )
+
+        st.caption("Los encuestados respondieron si padecian de alguna enfermedad crónica")
+
+        col23, col24= st.columns(2)
+            
+        col23.metric(f"{sexos[0]}",f"{cont_cronicidad.loc[sexos[0]]:.2f}%" )
+        col24.metric(f"{sexos[1]}",f"{cont_cronicidad.loc[sexos[1]]:.2f}%" )
+                                                                
+        
+       
+
+ ###### Tab 3 #######   
+     
+    with tab3:
+        st.header("Determinantes de salud: Dieta y actividad física")
+        st.caption("Los determinantes de salud son XYZ")
+
+        st.subheader("Frecuencia de alimentación")
+        st.caption("Promedio de veces a la semana")
+
+        carne_prom= df['Carne_frec'].mean()
+        refrescos_prom= df['Refrescos_frec'].mean()
+        embutidos_prom= df['Embutidos_frec'].mean()
+        lacteos_prom= df['Lacteos_frec'].mean()
+        verduras_prom= df['Verduras_frec'].mean()
+        st.markdown("----------")
+        col31, col32, col33, col34, col35= st.columns(5)
+
+        col31.metric(label="Carne", value=f"{lac_prom:.2f}")
+        col32.metric(label="Refrescos", value= f"{refrescos_prom:.2f}")
+        col33.metric(label= "Embutidos",value=f"{embutidos_prom:.2f}")
+        col34.metric(label= "Lacteos", value= f"{lacteos_prom:.2f}")
+        col35.metric(label= "Verduras", value= f"{verduras_prom:.2f}")
+
+        st.markdown("----------")
+
+        lista_frec= ["Carne", "Refrescos", "Embutidos", "Lacteos", "Verduras"]
+        frec_lista= {"Carne":"Carne_frec","Refrescos":"Refrescos_frec","Embutidos":"Embutidos_frec", "Lacteos":"Lacteos_frec", "Verduras":"Verduras_frec"}
+
+        opcion= st.selectbox("Selecciona un alimento", lista_frec)
+        col_df= frec_lista[opcion]
+        lista_colores={"Carne": "#ECDAB2","Refrescos": "#DDBFB3","Embutidos": "#9999C9","Lacteos": "#D8D8EA","Verduras": "#000078"}
+        colores=lista_colores[opcion]
+        frec_hst= alt.Chart(df).mark_bar(color=colores).encode(alt.X(col_df,bin=alt.Bin(maxbins=5), title= "Frecuencia de consumo semanal de "), alt.Y("count()", title= "Encuestados")).properties(width=600, height=400, title= "Consumo de "+opcion).interactive()
+        st.altair_chart(frec_hst,width='content')
+
+        st.markdown("----------")
     
         sedentarismo= alt.Chart(df).mark_bar().encode(alt.X("Sedentarismo%_horas:Q",bin= True, title= "Horas sentado"), alt.Y("count()")).properties(width=600, height=400).interactive()
 
-        st.altair_chart(sedentarismo,use_container_width=True)                                                            
+        st.altair_chart(sedentarismo,width='content')                                                            
 
                                                                     
-        st.dataframe(df.head()) #https://www.youtube.com/watch?v=7E3yxq-P-a8
+        
 
 ###########################################################################################################
 
