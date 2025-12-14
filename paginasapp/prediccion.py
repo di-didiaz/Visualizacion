f84d1082a729aa6876a59982101865b22cf3ab14
DiDi <dcdiazgo@uoc.edu>
2025-12-14 22:25:13 +0100

    Limpieza

--- paginasapp/prediccion.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Predicción de tu salud")
-st.write("Incluye los siguientes datos para que podamos predecir tu estado de salud percibido")
--- paginasapp/visualizaciondatos.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("📊 Cuáles fueron los resultados?")
-st.write("Exploremos lo que respondieron los encuestados en España sobre su salud en 2023")
