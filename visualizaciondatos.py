f9d1201d8018a581ea8b1d5fb50142fd5828c681
DiDi <dcdiazgo@uoc.edu>
2025-12-14 20:26:04 +0100

    limpieza

--- visualizaciondatos.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Predicciones", page_icon="⚕", layout="wide")
-
-
-st.title("📊 Cuáles fueron los resultados?")
-st.write("Exploremos lo que respondieron los encuestados en España sobre su salud en 2023")
